d62e509ef5a9941e895f6bf553d4e3783ab5fc86
Ally Taffet <142642596+allytaffet@users.noreply.github.com>
2023-08-20 19:31:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-test
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
